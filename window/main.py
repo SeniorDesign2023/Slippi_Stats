6c3c08dce99d42e7dff29be5791d99e7dea70db5
Zachary Crimmel <zcrimmel@gmail.com>
2023-11-10 16:20:44 -0700

    Slight tweak to see name of file

--- window/main.py
+++ window/main.py
@@ -12,14 +12,20 @@ def main():
     root.geometry('1280x760')
     root.title("Slippi Stats")
     Label(root, text = "Slippi Stats", font = ('Helvetica 20 bold')).pack(pady = 20)
-
+    
     # Create a File Explorer label
+    global label_file_explorer
+    label_file_explorer = Label(root, 
+                                text = "File Explorer using Tkinter",
+                                width = 100, height = 4, 
+                                fg = "blue")
     button_explore = Button(root, 
                             text = "Browse Files",
                             command = browseFiles)
+    label_file_explorer.pack(pady=10)
     button_explore.pack(pady=10)
     quitButton = ttk.Button(text="Quit", command=root.destroy)
-    quitButton.pack(pady = 10)
+    quitButton.pack(pady = 100)
     root.mainloop()
 
 
@@ -30,5 +36,5 @@ def browseFiles():
                                                         "*.slp*"),
                                                        ("all files",
                                                         "*.*")))
-    main.label_file_explorer.configure(text="File Opened: "+filename)
+    label_file_explorer.configure(text="File Opened: "+filename)
 main()
