fad35380b71e113768e3dedd156d967939c220fc
Zachary Crimmel <zcrimmel@gmail.com>
2024-02-27 22:17:11 -0700

    Added Image and dropdown functionality to main window

--- AI/.venv/Scripts/activate.bat
+++ AI/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\sonic\OneDrive - University of Wyoming\Fall 2023\Senior Design\Slippi_Stats\AI\.venv
+set VIRTUAL_ENV=C:\Users\zcrim\source\repos\SeniorDesign\Slippi_Stats\AI\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- window/main.py
+++ window/main.py
@@ -1,17 +1,21 @@
-### MAIN WINDOW ###
+### MAIN WINDOW ###Helvetica
 
 # Using tkinter version 8.6
 # Visit https://docs.python.org/3/library/tkinter.html for documentation
 
+import tkinter as tk
+import tkinter.ttk as ttk
 from tkinter import *
-from tkinter import ttk
 from tkinter import filedialog
+from PIL import Image, ImageTk
+import pathlib
+from pathlib import Path
 
 def main():
-    root = Tk()
+    root = tk.Tk()
     root.geometry('1280x760')
     root.title("Slippi Stats")
-    Label(root, text = "Slippi Stats", font = ('Helvetica 20 bold')).pack(pady = 20)
+    title = Label(root, text = "Slippi Stats", font = ('Helvetica 20 bold')).pack(pady = 20)
     
     # Create a File Explorer label
     global label_file_explorer
@@ -19,22 +23,57 @@ def main():
                                 text = "File Explorer using Tkinter",
                                 width = 100, height = 4, 
                                 fg = "blue")
+    
+    # Select Character
+    characters = ["Mario", "Bowser", "Peach", "Yoshi", "Donkey Kong",
+                  "Captain Falcon", "Fox", "Ness", "Ice Climbers",
+                  "Kirby", "Samus", "Zelda", "Link", "Pikachu",
+                  "Jigglypuff", "Dr. Mario", "Luigi", "Ganondorf",
+                  "Falco", "Young Link", "Pichu", "Mewtwo",
+                  "Mr. Game & Watch", "Marth", "Roy"]
+    selectedCharacter = tk.StringVar()
+    characterLabel = tk.Label(root, text="Select a character to train against")
+    characterLabel.pack(pady=10)
+    dropdown = tk.OptionMenu(root, selectedCharacter, *characters, command=characterImage(selectedCharacter))
+    dropdown.pack()
+
     button_explore = Button(root, 
                             text = "Browse Files",
                             command = browseFiles)
     label_file_explorer.pack(pady=10)
     button_explore.pack(pady=10)
-    quitButton = ttk.Button(text="Quit", command=root.destroy)
+    quitButton = tk.Button(text="Quit", command=root.destroy)
     quitButton.pack(pady = 100)
     root.mainloop()
 
+def characterImage(character):
+    if character is not None:
+        path = pathlib.Path(__file__).parent.resolve()
+        path = path._str.replace("window", "") + "CharacterImages\\Mario.jpg"
+        path = Path(path)
+        characterImage = Image.open(path)
+        test = ImageTk.PhotoImage(characterImage)
+        label1 = tk.Label(image=test)
+        label1.image = test
+        # Position image
+        label1.place(0, 0)
+    else:
+        path = pathlib.Path(__file__).parent.resolve()
+        path = path._str.replace("window", "") + "CharacterImages\\" + character.get() + ".jpg"
+        path = Path(path)
+        characterImage = Image.open(path)
+        test = ImageTk.PhotoImage(characterImage)
+        label1 = tk.Label(image=test)
+        label1.image = test
+        # Position image
+        label1.place(0, 0)
 
 def browseFiles():
     filename = filedialog.askopenfilename(initialdir = "/",
                                           title = "Select a File",
                                           filetypes = (("Slippi Files",
                                                         "*.slp*"),
-                                                       ("all files",
+                                                       ("All files",
                                                         "*.*")))
     label_file_explorer.configure(text="File Opened: "+filename)
 main()
