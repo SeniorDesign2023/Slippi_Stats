d0dd4764362bef37b047bbc2a49257aaf65a12de
arrangedcupid0 <79234342+arrangedcupid0@users.noreply.github.com>
2024-04-02 03:17:28 -0600

    myriad problems, but a functional bot.

--- AI/SlippiBot/michael_bot.py
+++ AI/SlippiBot/michael_bot.py
@@ -1,28 +1,72 @@
 import melee
 
-#mom said it's my turn to be player 1
-PORT_PLR = 1
-PORT_BOT = 2
-
+#mom said it's not my turn to be player 1
+PORT_BOT = 1
+PORT_HUMAN = 3
 #setup
 console = melee.Console(path="C:/Users/micha/AppData/Roaming/Slippi Launcher/netplay")
 
 controller = melee.Controller(console=console, port=PORT_BOT)
-controller_human = melee.Controller(console=console,
-                                    port=PORT_PLR,
-                                    type=melee.ControllerType.STANDARD)
+
+
 
 console.run()
 console.connect()
 
 
-controller_human.connect()
+
 controller.connect()
+#idea:
+#link is aggressive. he (vert/hrzt) pursues you until he is close enough to upB you
+#if he is out of melee range, he throws boomerangs while he approaches
+    #optional: he randomly decides which attack he does 
+#on your last stock, he taunts when you spawn
+
+#issues:
+#link doesn't see you if you are right behind him
+#link will happily chase you straight off the stage
+    #detect edge somehow?
+#also he lacks any sort of edge recovery
+    #ibid
+#link will simply kneel instead of dropping through platforms
+#when he approaches, instead of hitting he jumps, falls left, and boomerangs
+    #worth noting that he always falls left
+#he will not double jump
+    #i'm thinking make a framedata object because it has a method that checks double jump height
 
 while True:
     gamestate = console.step() 
     if gamestate.menu_state in [melee.Menu.IN_GAME, melee.Menu.SUDDEN_DEATH]:
-            pass
+            heightDif = gamestate.player[PORT_BOT].y - gamestate.player[PORT_HUMAN].y
+            sideDif = gamestate.player[PORT_BOT].x - gamestate.player[PORT_HUMAN].x
+            direction = gamestate.player[PORT_BOT].x < gamestate.player[PORT_HUMAN].x
+            if (gamestate.distance < 21): #if close, jump and up B. ps eat shit beckham 21 is better than 20
+                 #controller.release_button(melee.Button.BUTTON_MAIN)
+                 #controller.release_button(melee.Button.BUTTON_B)
+                 #controller.press_button(melee.Button.BUTTON_X)
+                 #controller.flush()
+                 controller.tilt_analog(melee.Button.BUTTON_MAIN, 0.5, 1)
+                 controller.press_button(melee.Button.BUTTON_B)
+            elif  (heightDif > -21) and (heightDif < 21): #if on the same height, shoot
+                 controller.release_button(melee.Button.BUTTON_MAIN)
+                 controller.simple_press(int(direction), 0.5, melee.Button.BUTTON_B)
+            else:
+                 if (sideDif > -21) and (sideDif < 21): #if in the same "column"
+                      if heightDif > 21: #player is lower
+                           #controller.release_button(melee.Button.BUTTON_MAIN)
+                           controller.release_button(melee.Button.BUTTON_X)
+                           controller.release_button(melee.Button.BUTTON_B)
+                           controller.tilt_analog(melee.Button.BUTTON_MAIN, 0, 0)
+                      elif heightDif < -21: #player is higher
+                            #controller.release_button(melee.Button.BUTTON_MAIN)
+                            controller.release_button(melee.Button.BUTTON_X)
+                            controller.release_button(melee.Button.BUTTON_B)
+                            controller.press_button(melee.Button.BUTTON_Y)
+                 else: #not the same height. find which direction to go and go there
+                     controller.tilt_analog(melee.Button.BUTTON_MAIN, int(direction), 0.5)
+                     controller.release_button(melee.Button.BUTTON_X)
+                     controller.release_button(melee.Button.BUTTON_B)
+                     controller.release_button(melee.Button.BUTTON_Y)
     else:
         melee.MenuHelper.menu_helper_simple(gamestate,
                                             controller,
@@ -31,4 +75,4 @@ while True:
                                             "",
                                             costume=0,
                                             autostart=False,
-                                            swag=True)
+                                            swag=False)
