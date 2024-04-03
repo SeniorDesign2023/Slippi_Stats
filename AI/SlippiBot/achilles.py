bd0da4f9c43b02bcd32ac7e7a162762581b6b608
arrangedcupid0 <79234342+arrangedcupid0@users.noreply.github.com>
2024-04-03 03:21:57 -0600

    renamed

--- AI/SlippiBot/achilles.py
+++ AI/SlippiBot/achilles.py
@@ -1,3 +1,7 @@
+##
+## Created by: Michael Stoll
+## Last Updated: 4/3/2024 12:31am
+##
 import melee
 import melee.gamestate
 
@@ -6,35 +10,30 @@ PORT_BOT = 1
 PORT_HUMAN = 2
 #setup
 console = melee.Console(path="C:/Users/micha/AppData/Roaming/Slippi Launcher/netplay")
-
 controller = melee.Controller(console=console, port=PORT_BOT)
-
-
-
 console.run()
 console.connect()
-
-
-
 controller.connect()
+
 #idea:
-#link is aggressive. he (vert/hrzt) pursues you until he is close enough to attack you with C
+#link is aggressive. he (vert/hrzt) pursues you until he is close enough to attack you with C and A
 #if he is out of melee range, he throws boomerangs while he approaches
     #optional: he randomly decides which attack he does 
-#on your last stock, he taunts when you spawn
+#if he falls off the stage, he up-B's to recover, with the intention of hitting you 
+#on your last stock, he taunts while you spawn
+#the fight takes place in the Pokemon Stadium
 
 #issues:
-#link doesn't see you if you are right behind him
-    #for that matter, he does nothing if you are right next to him on either side
-#sometimes, unprompted, he just runs off one of the sides and tries to kill himself
-    #relatable
 #he won't edge recover until he falls
 #link will simply kneel instead of dropping through platforms
-#sometimes he freezes somewhere in the second if
-#he will not double jump
+#the player can camp behind him if he begins a combo while facing away from them
+    #as soon as they move or attack, the spell is broken
+#sometimes he temporarily freezes somewhere in the second "if"
+#he will not double jump to save himself from the void
     #i'm thinking make a framedata object because it has a method that checks double jump height
-#he will not taunt
-print("here we go")
+    #or, and hear me out, this is his achilles' heel
+
+print("rise, achilles")
 mocked = False
 stagnant = False
 while True:
@@ -47,36 +46,30 @@ while True:
             controller.release_button(melee.Button.BUTTON_A)
             controller.release_button(melee.Button.BUTTON_C)
             if (gamestate.distance < 15): #if close, throw cstick
-                 print("first if. should C")
                  controller.release_button(melee.Button.BUTTON_B)
                  controller.tilt_analog(melee.Button.BUTTON_C, int(xdirection), int(ydirection))
                  if stagnant:
-                      print("was stagnant. attacking with A")
                       controller.release_button(melee.Button.BUTTON_C)
                       controller.press_button(melee.Button.BUTTON_A)
                       controller.tilt_analog(melee.Button.BUTTON_MAIN, int(xdirection), .5)
                       stagnant = False
                  else : 
-                      print("wasn't stagnant. is now")
                       stagnant = True
+                      #remember to turn and face your opponent
                       if gamestate.players[PORT_BOT].facing != int(xdirection):
                            controller.tilt_analog(melee.Button.BUTTON_MAIN, int(xdirection), .5)
                       else:
                            controller.release_button(melee.Button.BUTTON_MAIN)
 
             elif (heightDif > -5) and (heightDif < 5): #if on the same height
-                 print("2nd if. same height")
                  #controller.release_button(melee.Button.BUTTON_MAIN)
                  controller.release_button(melee.Button.BUTTON_C)
                  #if the bot is between the stage borders
                  if(gamestate.players[PORT_BOT].x > ((melee.stages.EDGE_GROUND_POSITION[melee.Stage.POKEMON_STADIUM] * -1) + 42)) and ((gamestate.players[PORT_BOT].x < melee.stages.EDGE_GROUND_POSITION[melee.Stage.POKEMON_STADIUM]) - 42):
-                    print("on the stage")
                     if gamestate.distance > 30: #if far away, throw boomerang
-                         print("throw boomy")
                          controller.press_button(melee.Button.BUTTON_B)
                          controller.tilt_analog(melee.Button.BUTTON_MAIN, int(xdirection), 0.5) #run at player
                     else: 
-                         print("stand and fight")
                          controller.tilt_analog(melee.Button.BUTTON_MAIN, int(xdirection), .5)
                          controller.tilt_analog(melee.Button.BUTTON_C, int(xdirection), int(ydirection))
             #if edge_hanging currently
@@ -95,48 +88,37 @@ while True:
                      controller.tilt_analog(melee.Button.BUTTON_MAIN, 0, .5)
 
             elif gamestate.players[PORT_BOT].off_stage: #if we've fallen off
-                print("third if. off stage")
                 if gamestate.players[PORT_BOT].y < -21: #if we're below the stage level
-                    print("fell below stage")
                     if gamestate.players[PORT_BOT].x < ((melee.stages.EDGE_POSITION[melee.Stage.POKEMON_STADIUM] * -1) + 42): #left side
-                        print("fell off left edge. jumping back")
                         controller.release_button(melee.Button.BUTTON_MAIN)
                         controller.simple_press(1, 1, melee.Button.BUTTON_B)
                     elif gamestate.players[PORT_BOT].x > melee.stages.EDGE_POSITION[melee.Stage.POKEMON_STADIUM] - 42: #right side
-                        print("fell off right edge. jumping back")
                         controller.release_button(melee.Button.BUTTON_MAIN)
                         controller.simple_press(0, 1, melee.Button.BUTTON_B)
                 else: #if we've just barely walked off
-                    print("just off stage")
+                    #in theory, these would enable you to just pop up real quick.
+                    #in practice, nothing happens
                     if gamestate.players[PORT_BOT].x < ((melee.stages.EDGE_POSITION[melee.Stage.POKEMON_STADIUM] * -1) + 42): #left side
-                        print("fell off left edge. eeking back")
                         controller.press_button(melee.Button.BUTTON_Y)
                         controller.tilt_analog(melee.Button.BUTTON_MAIN, 1, .5)
                     elif gamestate.players[PORT_BOT].x > melee.stages.EDGE_POSITION[melee.Stage.POKEMON_STADIUM] - 42: #right side
-                        print("fell off right edge. eeking back")
                         controller.press_button(melee.Button.BUTTON_Y)
                         controller.tilt_analog(melee.Button.BUTTON_MAIN, 0, .5)
-
             elif (gamestate.players[PORT_HUMAN].stock > 1) or mocked: #just to make sure we've taunted if the person is down to 1 stock
-                 print("time to chase")
                  if (sideDif > -21) and (sideDif < 21): #if in the same "column"
-                      print("in the column")
                       if heightDif > 5: #player is lower
-                           print("player lower")
                            controller.release_all()
                            controller.tilt_analog(melee.Button.BUTTON_MAIN, 0.5, 0)
+                           controller.tilt_analog(melee.Button.BUTTON_MAIN, 0.5, 0)
                       elif heightDif < -5: #player is higher
-                            print("player higher")
                             controller.release_all()
                             controller.press_button(melee.Button.BUTTON_Y)
                             controller.tilt_analog(melee.Button.BUTTON_MAIN, 0.5, 1)
                  else: #not the same column. find which xdirection to go and go there
-                     print("find the column")
                      controller.release_all()
                      controller.tilt_analog(melee.Button.BUTTON_MAIN, int(xdirection), 0.5)
             
-            else: #one stock left. nerd
-                 print("down to one stock. should taunt")
+            else: #one stock left. time to meme
                  mocked = True
                  controller.release_all()
                  controller.press_button(melee.Button.BUTTON_D_UP)
